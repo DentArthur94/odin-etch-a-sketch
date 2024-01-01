2265c80824e561f1291231dce22d35a789c32f25
DentArthur94 <73761376+DentArthur94@users.noreply.github.com>
2024-01-01 14:52:55 +0100

    Setup the grid function

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 "use strict"; 
 
+const container = document.querySelector("#container"); 
+
+
+
+
+createGrid(4,4);
+
+
+
+
+function addNUmberOfLine(numberOfLine) {
+    for(let i = 0 ; i < numberOfLine ; i++) {
+        addLine();
+    }
+};
+
+function addLine() {
+    const line = document.createElement("div");
+    line.setAttribute("class","line"); 
+    container.appendChild(line);
+};
+
+
+function addColumn() {
+    const lines = document.querySelectorAll(".line");
+    lines.forEach(
+        (line) => {
+            const column = document.createElement("div");
+            column.setAttribute("class","column");
+            line.appendChild(column)
+        }
+
+    );
+};
+
+
+function addNumberOfColumn(numberOfColumn) {
+    for(let i = 0 ; i < numberOfColumn ; i++) {
+        addColumn();
+    };
+};
+
+function createGrid(nbOfLines, nbOfColumns) {
+    addNUmberOfLine(nbOfLines);
+    addNumberOfColumn(nbOfColumns);
+}
+
+
