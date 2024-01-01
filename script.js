10cd3e4df5562485642ea2fd5a3507bdc4b7e268
DentArthur94 <73761376+DentArthur94@users.noreply.github.com>
2024-01-01 15:39:23 +0100

    Add feature for leaving a trail on hover

--- script.js
+++ script.js
@@ -2,24 +2,40 @@
 
 const container = document.querySelector("#container"); 
 
+createGrid(50,50);
+
+const gridBlock = document.querySelectorAll(".column"); 
+
+gridBlock.forEach( (block) => {
+
+    block.addEventListener("mouseenter", function eventHandler() {
+
+        block.style.cssText = 'background-color : blue;';
+
+        setTimeout(function () {
+            block.style.cssText = '';
+        }, 500);
+
+        
+    });
+});
 
 
 
-createGrid(4,4);
 
 
 
-
-function addNUmberOfLine(numberOfLine) {
-    for(let i = 0 ; i < numberOfLine ; i++) {
-        addLine();
-    }
-};
 
 function addLine() {
     const line = document.createElement("div");
     line.setAttribute("class","line"); 
     container.appendChild(line);
+};
+
+function addNUmberOfLine(numberOfLine) {
+    for(let i = 0 ; i < numberOfLine ; i++) {
+        addLine();
+    }
 };
 
 
